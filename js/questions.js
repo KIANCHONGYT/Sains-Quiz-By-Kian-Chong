53d6b5c2d00c7bbce97f04f6e7c56a29a3e51146
KIANCHONGYT <74892679+KIANCHONGYT@users.noreply.github.com>
2021-06-23 15:22:56 +0800

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Dua atau lebih laluan untuk tenaga elektrik mengalir.Mentol-mentol disusun dalam laluan berlainan. Jika satu mentol terbakar, mentol satu lagi akan terus menyala. Apakah jenis litar yang memenuhi spesifikasi ini?",
+    answer: "litar selari",
+    options: [
+      "litar siri",
+      "litar selari",
+      "litar siri selari",
+      "litar pintas"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Berapakah voltan yang diperlukan untuk mengalirkan arus elektrik 2A melalui perintang 10Ω?",
+    answer: "20V",
+    options: [
+      "5V",
+      "0.2V",
+      "20V",
+      "200V"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Apakah huruf yang digunakan untuk mewakili arus?",
+    answer: "I",
+    options: [
+      "A",
+      "I",
+      "R",
+      "E"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Apakah rumus untuk mengira voltan?",
+    answer: "arus X rintangan",
+    options: [
+      "arus X rintangan",
+      "rintangan ÷ arus",
+      "arus ÷ rintangan",
+      "ukur dengan pembaris"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Arus elektrik diukur dalam unit",
+    answer: "A (ampere)",
+    options: [
+      "Ω (ohms)",
+      "V (volts)",
+      "A (ampere)",
+      "W (watts)"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Yang manakah menunjukkan persamaan antara asid dan alkali?",
+    answer: "Mengakis",
+    options: [
+      "Mengakis",
+      "Mempunyai rasa masam",
+      "Mempunyai pH lebih daripada 7",
+      "Menukarkan kertas litmus biru kepada merah"
+    ]
+  },
+      {
+    numb: 7,
+    question: "Berapakah rintangan yang diperlukan untuk mengalir arus elektrik 20A dengan bateri 100V?",
+    answer: "5Ώ",
+    options: [
+      "0.2Ώ",
+      "2000Ώ",
+      "120Ώ",
+      "5Ώ"
+    ]
+  },
+        {
+    numb: 8,
+    question: "Apakah saiz perintang yang akan menghasilkan arus 3 A melalui bateri 12 V",
+    answer: "4 Ω",
+    options: [
+      "1 Ω",
+      "3 Ω",
+      "4 Ω",
+      "12 Ω"
+    ]
+  },
+        {
+    numb: 9,
+    question: "Bahan yang manakah merupakan kategori asid?",
+    answer: "asid hidroklorik",
+    options: [
+      "natrium hidroksida",
+      "asid hidroklorik",
+      "kalsium hidroksida",
+      "kalium hidroksida"
+    ]
+  },
+        {
+    numb: 10,
+    question: "pH 8 adalah ....... daripada pH 13.",
+    answer: "Kurang beralkali",
+    options: [
+      "Kurang berasid",
+      "Kurang beralkali",
+      "Lebih berasid",
+	  "Lebih beralkali",
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 11,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
